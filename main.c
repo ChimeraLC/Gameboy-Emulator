3edf53e392f4947e5534b0f306d6f2c9f8e000e8
ChimeraLC <lucaschen314@gmail.com>
2023-08-02 20:50:40 -0500

    Fixed main memory and added interrupts

--- main.c
+++ main.c
@@ -6,20 +6,58 @@
 #include <unistd.h>
 
 /*
- * CPU emulation
+ *      Constants definitions
+ */
+#define ROM_ADDR 0x0000
+#define VRAM_ADDR 0x8000
+#define ERAM_ADDR 0xA000
+#define WRAM_ADDR 0xC000
+#define OAM_ADDR 0xFE00
+#define IOR_ADDR 0xFF00
+#define HRAM_ADDR 0xFF80
+
+/*
+ *      Memory
  */
 
-// Memory
-#define MRAM_SIZE 0x2000        // Main RAM
-#define VRAM_SIZE 0x2000        // Video RAM
+uint8_t *ROM;           // ROM from cartridge
+uint8_t VRAM[0x4000];   // Virtual RAM
+uint8_t ERAM[0x2000];   // External RAM
+uint8_t WRAM[0x8000];   // Working RAM
+uint8_t OAM[0xA0];      // Object Attribute Memory
+uint8_t IOR[0x80];      // I/O Registers
+uint8_t HRAM[0x7F];     // High RAM
 
-uint8_t memory[0xFFFF];         // Entirety of memory (may need to be larger)
-uint8_t rom;
+uint8_t rom_bank;       // Switchable ROM bank via mapper
+uint8_t vram_bank;      // Switchable VRAM bank 0/1 (CBG only)
+uint8_t eram_bank;      // Switchable ERAM bank if any
+uint8_t wram_bank;      // Switchable WRAM bank 1-7
 
-// Function heads
-void init();
+/*
+ *      Specific Stuff
+ */
+static uint8_t BIOS[0x100] = {
+	0x31, 0xFE, 0xFF, 0xAF, 0x21, 0xFF, 0x9F, 0x32, 0xCB, 0x7C, 0x20, 0xFB, 0x21, 0x26, 0xFF, 0x0E,
+	0x11, 0x3E, 0x80, 0x32, 0xE2, 0x0C, 0x3E, 0xF3, 0xE2, 0x32, 0x3E, 0x77, 0x77, 0x3E, 0xFC, 0xE0,
+	0x47, 0x11, 0x04, 0x01, 0x21, 0x10, 0x80, 0x1A, 0xCD, 0x95, 0x00, 0xCD, 0x96, 0x00, 0x13, 0x7B,
+	0xFE, 0x34, 0x20, 0xF3, 0x11, 0xD8, 0x00, 0x06, 0x08, 0x1A, 0x13, 0x22, 0x23, 0x05, 0x20, 0xF9,
+	0x3E, 0x19, 0xEA, 0x10, 0x99, 0x21, 0x2F, 0x99, 0x0E, 0x0C, 0x3D, 0x28, 0x08, 0x32, 0x0D, 0x20,
+	0xF9, 0x2E, 0x0F, 0x18, 0xF3, 0x67, 0x3E, 0x64, 0x57, 0xE0, 0x42, 0x3E, 0x91, 0xE0, 0x40, 0x04,
+	0x1E, 0x02, 0x0E, 0x0C, 0xF0, 0x44, 0xFE, 0x90, 0x20, 0xFA, 0x0D, 0x20, 0xF7, 0x1D, 0x20, 0xF2,
+	0x0E, 0x13, 0x24, 0x7C, 0x1E, 0x83, 0xFE, 0x62, 0x28, 0x06, 0x1E, 0xC1, 0xFE, 0x64, 0x20, 0x06,
+	0x7B, 0xE2, 0x0C, 0x3E, 0x87, 0xF2, 0xF0, 0x42, 0x90, 0xE0, 0x42, 0x15, 0x20, 0xD2, 0x05, 0x20,
+	0x4F, 0x16, 0x20, 0x18, 0xCB, 0x4F, 0x06, 0x04, 0xC5, 0xCB, 0x11, 0x17, 0xC1, 0xCB, 0x11, 0x17,
+	0x05, 0x20, 0xF5, 0x22, 0x23, 0x22, 0x23, 0xC9, 0xCE, 0xED, 0x66, 0x66, 0xCC, 0x0D, 0x00, 0x0B,
+	0x03, 0x73, 0x00, 0x83, 0x00, 0x0C, 0x00, 0x0D, 0x00, 0x08, 0x11, 0x1F, 0x88, 0x89, 0x00, 0x0E,
+	0xDC, 0xCC, 0x6E, 0xE6, 0xDD, 0xDD, 0xD9, 0x99, 0xBB, 0xBB, 0x67, 0x63, 0x6E, 0x0E, 0xEC, 0xCC,
+	0xDD, 0xDC, 0x99, 0x9F, 0xBB, 0xB9, 0x33, 0x3E, 0x3c, 0x42, 0xB9, 0xA5, 0xB9, 0xA5, 0x42, 0x4C,
+	0x21, 0x04, 0x01, 0x11, 0xA8, 0x00, 0x1A, 0x13, 0xBE, 0x20, 0xFE, 0x23, 0x7D, 0xFE, 0x34, 0x20,
+	0xF5, 0x06, 0x19, 0x78, 0x86, 0x23, 0x05, 0x20, 0xFB, 0x86, 0x20, 0xFE, 0x3E, 0x01, 0xE0, 0x50 };
 
-// Registers
+/*
+ *      Registers
+ */
+
 struct registers                      // 8-bit registers
 {
         union {
@@ -57,6 +95,10 @@ uint16_t PC;                    // Program counter
 uint16_t SP;                    // Stack pointer
 uint8_t opcode;                 // Current opcode
 uint8_t cbcode;                 // Opcode for 2 byte operations
+uint8_t IME;                    // Interrupt Master Flag
+uint8_t IE;                     // Interrupt Enable
+uint8_t IF;                     // Interrupt Flag
+uint8_t HALT;                   // HALT flag
 
 // Random values
 uint8_t *r1;
@@ -64,7 +106,17 @@ uint8_t *r2;
 uint8_t n;
 uint16_t nn;
 uint32_t nnnn;
+bool active = true;
 
+/*
+ *      Function definitions
+ */
+
+void init_mem();
+uint8_t read_mem(uint16_t addr);
+void write_mem(uint16_t addr, uint8_t val);
+void read_rom(char *filename);
+void execute();
 
 int
 main(int argc, char **argv)
@@ -76,7 +128,7 @@ main(int argc, char **argv)
 	}
 
         // Initializing memory
-        init();
+        init_mem();
 
         // Initialize SDL
 
@@ -86,15 +138,65 @@ main(int argc, char **argv)
 
         read_rom(filename);
 
+        // Startup Sequence
+        (void) BIOS;
+
+        // Emulating cycles
+        while (active)
+        {
+                // Handle interrupts
+                if (IME && (IE & IF)) {         // Check for correspondings flags
+                        // Put PC on stack
+                        write_mem(--SP, PC >> 8);
+                        write_mem(--SP, PC & 0xff);
+
+                        // Vblank
+                        if (IE & IF & 0x1) {
+                                PC = 0x40;
+                                IF &= ~(0x1);
+                        }
+
+                        // LCD STAT
+                        if (IE & IF & 0x2) {
+                                PC = 0x48;
+                                IF &= ~(0x2);
+                        }
+
+                        // Timer
+                        if (IE & IF & 0x4) {
+                                PC = 0x50;
+                                IF &= ~(0x4);
+                        }
+
+                        // Serial
+                        if (IE & IF & 0x8) {
+                                PC = 0x58;
+                                IF &= ~(0x8);
+                        }
+
+                        // Joypad
+                        if (IE & IF & 0x10) {
+                                PC = 0x60;
+                                IF &= ~(0x10);
+                        }
+                }
+
+                opcode = read_mem(PC++);        // Read opcode
+
+                execute();                      // Execute current opcode
+
+                // TODO: delay to sync hardware cycles
+        }
+
         return 1;
 }
 
-
-
 void
-init()
+init_mem()
 {
-        memset(memory, 0, 0x2000);
+        /*
+
+        memset(VRAM, 0, 0x2000);
         // Initial register and pointer values
         PC = 0x0000;
         SP = 0xFFFE;
@@ -135,8 +237,108 @@ init()
         memory[0xFF4A] = 0x00;
         memory[0xFF4B] = 0x00;
         memory[0xFFFF] = 0x00;
-        
         return;
+        */
+}
+
+uint8_t
+read_mem(uint16_t addr)
+{
+        switch (addr & 0xF000) {
+                case 0x0000:
+                case 0x1000:
+                case 0x2000:
+                case 0x3000:
+                return ROM[addr - ROM_ADDR];
+                break;
+                case 0x4000:
+                case 0x5000:
+                case 0x6000:
+                case 0x7000:
+                return ROM[addr - ROM_ADDR + (rom_bank - 1) * 0x4000];
+                break;
+                case 0x8000:
+                case 0x9000:
+                return VRAM[addr - VRAM_ADDR + (vram_bank) * 0x2000];
+                break;
+                case 0xA000:
+                case 0xB000:
+                return ERAM[addr - ERAM_ADDR + (eram_bank) * 0x2000];
+                break;
+                case 0xC000:
+                return WRAM[addr - WRAM_ADDR];
+                break;
+                case 0xD000:
+                return WRAM[addr - WRAM_ADDR + (wram_bank - 1) * 0x1000];
+                break;
+                case 0xE000:
+                return WRAM[addr - WRAM_ADDR - 0x2000];
+                break;
+                case 0xF000:
+                if (addr < OAM_ADDR)
+                        return WRAM[addr - WRAM_ADDR + (wram_bank - 1) * 0x1000 - 0x2000];
+                if (addr < 0xFEA0)
+                        return OAM[addr - OAM_ADDR];
+                if (addr < IOR_ADDR)
+                        return 1;
+                if (addr < HRAM_ADDR)
+                        return IOR[addr - IOR_ADDR];
+                if (addr < 0xFFFF)
+                        return HRAM[addr - HRAM_ADDR];
+                return IE;
+                break;
+        }
+
+        return 1;
+}
+
+void
+write_mem(uint16_t addr, uint8_t val)
+{
+        switch (addr & 0xF000) {
+                case 0x0000:
+                case 0x1000:
+                case 0x2000:
+                case 0x3000:
+                return;
+                break;
+                case 0x4000:
+                case 0x5000:
+                case 0x6000:
+                case 0x7000:
+                return;
+                break;
+                case 0x8000:
+                case 0x9000:
+                VRAM[addr - VRAM_ADDR + (vram_bank) * 0x2000] = val;
+                break;
+                case 0xA000:
+                case 0xB000:
+                ERAM[addr - ERAM_ADDR + (eram_bank) * 0x2000] = val;
+                break;
+                case 0xC000:
+                WRAM[addr - WRAM_ADDR] = val;
+                break;
+                case 0xD000:
+                WRAM[addr - WRAM_ADDR + (wram_bank - 1) * 0x1000] = val;
+                break;
+                case 0xE000:
+                WRAM[addr - WRAM_ADDR - 0x2000] = val;
+                break;
+                case 0xF000:
+                if (addr < OAM_ADDR)
+                        WRAM[addr - WRAM_ADDR + (wram_bank - 1) * 0x1000 - 0x2000] = val;
+                if (addr < 0xFEA0)
+                        OAM[addr - OAM_ADDR] = val;
+                if (addr < IOR_ADDR)
+                        return;
+                if (addr < HRAM_ADDR)
+                        IOR[addr - IOR_ADDR] = val;
+                if (addr < 0xFFFF)
+                        HRAM[addr - HRAM_ADDR] = val;
+                IE = val;
+                break;
+        }
 }
 
 void
@@ -146,74 +348,24 @@ read_rom(char *filename)
 
         if (rom_file == NULL) {
                 printf("Error opening provided filename\n");
-                return(-1);
+                return;
         }
 
         // Copying into memory
         fseek(rom_file, 0, SEEK_END);
-        long fsize = ftell(rom);
+        long fsize = ftell(rom_file);
         fseek(rom_file, 0, SEEK_SET); 
-        rom = (uint8_t*)malloc(fsize);
-        fread(rom, fsize, 1, rom_file);
+        ROM = (uint8_t*)malloc(fsize);
+        fread(ROM, fsize, 1, rom_file);
         fclose(rom_file);
 
-        /*
-        uint16_t opcode = memory[0] << 8 | memory[1];
-        printf("%X", opcode);
-        */
 }
 
-uint8_t
-read_mem(uint16_t addr)
-{
-        return memory[addr];
-}
+
 
 void
-write_mem(uint16_t addr, uint8_t val)
-{
-        memory[addr] = val;
-        switch (addr & 0xF000) {
-                case 0x0000:
-                break;
-                case 0x1000:
-                break;
-                case 0x2000:
-                break;
-                case 0x3000:
-                break;
-                case 0x4000:
-                break;
-                case 0x5000:
-                break;
-                case 0x6000:
-                break;
-                case 0x7000:
-                break;
-                case 0x8000:
-                break;
-                case 0x9000:
-                break;
-                case 0xA000:
-                break;
-                case 0xB000:
-                break;
-                case 0xC000:
-                break;
-                case 0xD000:
-                break;
-                case 0xE000:
-                break;
-                case 0xF000:
-                break;
-        }
-}
-
-void
-cycle()                                                                         // TODO: fix references to (HL) to be accurate
+execute()                                                                         // TODO: fix references to (HL) to be accurate
 {                                                                               // TODO: still missing misc, rotates, bit opcodes (3.3.5, 3.3.6, 3.3.7)
-        opcode = read_mem(PC++);
-
         // Halting
         if (opcode == 0x76) {
                 return;                 // HALT
