a8c101636c501ca12b2ad44d1eba7bb96aab0aa9
ChimeraLC <lucaschen314@gmail.com>
2023-08-10 20:39:18 -0500

    Update main.h

--- main.h
+++ main.h
@@ -1,9 +1,18 @@
 #include <unistd.h>
 
 /*
+ * Shared variables
+ */
+extern int verbose;
+
+/*
  * Function headers
  */
 
 int read_rom(char *filename);
 int init_SDL();
 uint8_t get_joystick();
+
+
+#define MIN(a, b)   ((a) < (b) ? (a) : (b))
+#define MAX(a, b)   ((a) < (b) ? (b) : (a))
